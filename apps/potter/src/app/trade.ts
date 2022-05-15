7fce3baa090e4735674e22d69d4e320c16519e0b
kn71026 <nicolechou9712@gmail.com>
2022-05-16 00:39:32 +0800

    implement first test case

--- apps/potter/src/app/trade.ts
+++ apps/potter/src/app/trade.ts
@@ -1,8 +1,12 @@
 export class Trade {
+  private _price = 8;
+  private _totalPrice = 0;
 
-  checkout(bucket: number[]) { }
+  checkout(bucket: number[]) {
+    this._totalPrice += bucket.length * this._price;
+  }
 
   get price() {
-    return -1;
+    return this._totalPrice;
   }
 }
