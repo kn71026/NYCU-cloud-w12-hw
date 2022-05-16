a2dab393e66190690e15d3a87a07785119e8c633
kn71026 <nicolechou9712@gmail.com>
2022-05-16 10:41:51 +0800

    create fifth test case

--- apps/potter/src/app/trade.spec.ts
+++ apps/potter/src/app/trade.spec.ts
@@ -29,4 +29,10 @@ describe('Trade', () => {
     expect(trade.price).toBe(8 * 4 * 0.8 + 8 * 2 * 0.95);
   });
 
+  // fifth test edge case
+  test('fifth test edge case', () => {
+    trade.checkout([1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5]);
+    expect(trade.price).toBe(3 * (8 * 5 * 0.75) + 2 * (8 * 4 * 0.8));
+  });
+
 });
